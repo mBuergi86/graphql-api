f7b7e839ae47d61ffaaa2dc3206bc642a2ce19ac
Markus Bürgi <117123240+mBuergi86@users.noreply.github.com>
2024-04-01 12:36:26 +0200

    refactor: ♻️ Refactor createUser, updateUser, and deleteUser resolvers

--- src/lib/backend/api/graphql/resolvers/Mutation.ts
+++ src/lib/backend/api/graphql/resolvers/Mutation.ts
@@ -1,20 +1,58 @@
-import { User, type UsersDataSource } from '$lib/backend/mongodb/models/users';
+import { type UsersDataSource } from '$lib/backend/mongodb/models/users';
 import type { YogaInitialContext } from 'graphql-yoga';
 import type { UserInput } from '../types/user.types';
 import { type GraphQLFieldResolver } from 'graphql';
+import mongoose from 'mongoose';
 
 interface ResolverContext extends YogaInitialContext {
 	users: UsersDataSource;
 }
 
 export const Mutation = {
-	createUser: (async (parent: unknown, { userInput }: { userInput: UserInput }, { users }) => {
+	createUser: (async (
+		parent: unknown,
+		{ userInput }: { userInput: UserInput },
+		{ users }: { users: UsersDataSource }
+	) => {
 		try {
 			const userId = await users.create(userInput);
 
-			return userId;
+			return userId.toString();
 		} catch (error) {
-			throw new Error('Failed to create user');
+			throw new Error(`Failed to create user: ${error}`);
 		}
-	}) satisfies GraphQLFieldResolver<unknown, ResolverContext, { userInput: UserInput }>
+	}) satisfies GraphQLFieldResolver<unknown, ResolverContext, { userInput: UserInput }>,
+
+	updateUser: (async (
+		parent: unknown,
+		{ _id, userInput }: { _id: string; userInput: UserInput },
+		{ users }
+	) => {
+		console.log(_id);
+		try {
+			const newId: mongoose.Schema.Types.ObjectId =
+				_id as unknown as mongoose.Schema.Types.ObjectId;
+			await users.update(newId, userInput);
+
+			return _id;
+		} catch (error) {
+			throw new Error(`Failed to update user ${error}: ${_id}`);
+		}
+	}) satisfies GraphQLFieldResolver<
+		unknown,
+		ResolverContext,
+		{ _id: string; userInput: UserInput }
+	>,
+
+	deleteUser: (async (parent: unknown, { _id }: { _id: string }, { users }) => {
+		try {
+			const newId: mongoose.Schema.Types.ObjectId =
+				_id as unknown as mongoose.Schema.Types.ObjectId;
+			await users.remove(newId);
+
+			return _id;
+		} catch (error) {
+			throw new Error(`Failed to delete user ${error}`);
+		}
+	}) satisfies GraphQLFieldResolver<unknown, ResolverContext, { _id: string }>
 };
