feefe8d65b52a79638938c405d4f99e6c97193e0
Markus Bürgi <117123240+mBuergi86@users.noreply.github.com>
2024-04-01 12:36:42 +0200

    refactor: ♻️ Refactor getUserById and getUsersAll resolvers

--- src/lib/backend/api/graphql/resolvers/Query.ts
+++ src/lib/backend/api/graphql/resolvers/Query.ts
@@ -1,18 +1,18 @@
-import { User } from "$lib/backend/mongodb/models/users";
+import { User } from '$lib/backend/mongodb/models/users';
 
 export const Query = {
-    getUsersAll: async () => {
-        try {
-            return await User.find();
-        } catch (error) {
-            throw new Error('Failed to fetch users');
-        }
-    },
-    getUserById: async (_: unknown, { id }: { id: string }) => {
-        try {
-            return await User.findById(id);
-        } catch (error) {
-            throw new Error('Failed to fetch user');
-        }
-    }
+	getUsersAll: async (_: unknown, { limit }: { limit: number }) => {
+		try {
+			return await User.find().limit(limit);
+		} catch (error) {
+			throw new Error('Failed to fetch users');
+		}
+	},
+	getUserById: async (_: unknown, { _id }: { _id: string }) => {
+		try {
+			return await User.findById(_id);
+		} catch (error) {
+			throw new Error('Failed to fetch user');
+		}
+	}
 };
