94f4c10a73bdef167dbeadbb882d8cb8bae0693a
Markus Bürgi <117123240+mBuergi86@users.noreply.github.com>
2024-04-01 12:37:51 +0200

    refactor: ♻️ Add update and remove functions to UsersDataSource

--- src/lib/backend/mongodb/models/users.ts
+++ src/lib/backend/mongodb/models/users.ts
@@ -1,5 +1,5 @@
 import type { UserInput } from '$lib/backend/api/graphql/types/user.types';
-import { Schema, model, type InferSchemaType } from 'mongoose';
+import mongoose, { Schema, model, type InferSchemaType, type ObjectId } from 'mongoose';
 
 const userSchema = new Schema({
 	firstname: { type: String, required: true },
@@ -16,6 +16,10 @@ const userSchema = new Schema({
 type User = InferSchemaType<typeof userSchema>;
 
 export const User = model('User', userSchema);
+
+const filter = (id: ObjectId) => ({
+	_id: id
+});
 
 const create = async (userInput: UserInput) => {
 	const newUser = new User({
@@ -35,13 +39,47 @@ const create = async (userInput: UserInput) => {
 	return savedUser._id.toString();
 };
 
+const update = async (id: ObjectId, userInput: UserInput) => {
+	if (!mongoose.isValidObjectId(id)) {
+		throw new Error('Invalid ID');
+	}
+
+	await User.findByIdAndUpdate(filter(id), {
+		$set: {
+			firstname: userInput.firstname,
+			lastname: userInput.lastname,
+			address: userInput.address,
+			number: userInput.number,
+			zip: userInput.zip,
+			city: userInput.city,
+			country: userInput.country,
+			birthdate: userInput.birthdate,
+			gender: userInput.gender
+		}
+	});
+};
+
+const remove = async (id: ObjectId) => {
+	if (!mongoose.isValidObjectId(id)) {
+		throw new Error('Invalid ID');
+	}
+
+	await User.deleteOne({
+		_id: id
+	});
+};
+
 export interface UsersDataSource {
 	create: (userInput: UserInput) => Promise<string>;
+	update: (id: ObjectId, userInput: UserInput) => Promise<void>;
+	remove: (id: ObjectId) => Promise<void>;
 }
 
 export const getUsersDataSource = (): UsersDataSource => {
 	const userDataSource = {
-		create
+		create,
+		update,
+		remove
 	};
 
 	return userDataSource;
